d1898effb8cad705d465a873094aa62fcca46d9e
Alejandro-131004 <130375044+Alejandro-131004@users.noreply.github.com>
2024-10-10 17:33:35 +0100

    limpeza de csvs

--- environment.py
+++ environment.py
@@ -1,7 +1,1 @@
-import pandas as pd
-
-# Leitura do arquivo CSV
-df = pd.read_csv('energy_dataset.csv')
-
-# Exibe as primeiras linhas do DataFrame para ver a estrutura
-print(df.head())
+class environment(self, )
