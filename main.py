0fc2d36236e445574d1daaca30951edd0ba24d0c
Jony700 <joaopids70@gmail.com>
2024-11-18 16:29:25 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import asyncio
 from agents.initialize_agents import start_agents
-from environment import Environment
 from datetime import datetime, timedelta
 
 def main():
@@ -10,29 +9,12 @@ def main():
     end_date = datetime.strptime('2015-01-01 23:00:00', '%Y-%m-%d %H:%M:%S')
     
     current_date = start_date
-     # Inicializa o ambiente
-    env = Environment(date=current_date.strftime('%Y-%m-%d %H:%M:%S'), city='Valencia', num_divisions=5, desired_temperature=24)
-    asyncio.run(start_agents(env))  # Passando env como argumento
-    # Loop até alcançar a data de término
-    while current_date <= end_date:
+   
+    asyncio.run(start_agents(date=current_date.strftime('%Y-%m-%d %H:%M:%S'), city='Valencia', num_divisions=5, desired_temperature=24))  # Passando env como argumento
+    
        
         
-        # Exibe os dados meteorológicos
-        env.display_weather_data()
-
-        # Obtém o preço atual da energia
-        current_price = env.get_price_for_current_hour()
-        print(f"Current energy price for {current_date}: {current_price}")
-
-        # Chama a função para obter os dados meteorológicos para a hora atual
-        current_weather = env.get_weather_for_each_hour()
-        print(f"Weather data for {current_date}: {current_weather}")
         
-        # Inicia os agentes, passando o ambiente para a função start_agents
-        
-        
-        # Avança uma hora no tempo
-        current_date += timedelta(hours=1)
 
 if __name__ == "__main__":
     main()
